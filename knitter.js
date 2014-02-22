9e0da664c3b968ee120cbb88b178e4a5587ee93b
Glen Oakley <goakley123@gmail.com>
2014-02-22 18:55:26 -0500

    Enabled use of 'regen sweater'

--- knitter.js
+++ knitter.js
@@ -1,4 +1,21 @@
 (function() {
+    document.getElementById("button_regen").addEventListener("click", function(e) {
+        e.target.disabled = true;
+        draw_sweater();
+    });
+    function draw_sweater() {
+        var canvas_source = document.getElementById("image_source");
+        var context = canvas_source.getContext("2d");
+        var imgobj = context.getImageData(0, 0, canvas_source.width, canvas_source.height);
+        imgobj = generate_sweater_from_image(imgobj);
+        var canvas_sweater = document.getElementById("image_sweater");
+        canvas_sweater.style.display = "inline";
+        canvas_sweater.width = imgobj.width;
+        canvas_sweater.height = imgobj.height;
+        context = canvas_sweater.getContext("2d");
+        context.putImageData(imgobj, 0, 0);
+        document.getElementById("button_regen").disabled = false;
+    }
     // use a drawable object to create the sweater
     function use_image_with_source(width, height, source) {
         var canvas_source = document.getElementById("image_source");
@@ -7,14 +24,7 @@
         canvas_source.height = height;
         var context = canvas_source.getContext("2d");
         context.drawImage(source, 0, 0, width, height);
-        var imgobj = context.getImageData(0, 0, width, height);
-        imgobj = generate_sweater_from_image(imgobj);
-        var canvas_sweater = document.getElementById("image_sweater");
-        canvas_sweater.style.display = "inline";
-        canvas_sweater.width = imgobj.width;
-        canvas_sweater.height = imgobj.width;
-        context = canvas_sweater.getContext("2d");
-        context.putImageData(imgobj, 0, 0);
+        draw_sweater();
     }
     // function to get camera approval
     function onclick_camera_approval(event) {
